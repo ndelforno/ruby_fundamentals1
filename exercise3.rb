ea10ff9d434ad426872c866139a6734794a3fbb8
Ndelforno <nicolas.delforno@gmail.com>
2018-09-04 15:32:36 -0400

    Exercise 3 done

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name ?"
-user_name = gets
+user_name = gets.chomp
 puts "Hello, #{user_name}"
+puts "How old are you ?"
+user_age = gets.chomp
+puts "You are #{user_age} years old, that means you are born in #{2018-user_age.to_i}"

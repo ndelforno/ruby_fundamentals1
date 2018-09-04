64dc03b5114e21ebedcdebdd3c3e0f57651b6890
Ndelforno <nicolas.delforno@gmail.com>
2018-09-04 17:46:29 -0400

    adding exercise 6.2

--- exercise5.rb
+++ exercise5.rb
@@ -1,19 +1,26 @@
 distance = 0
 home = true
+energy = 100
 until  home == false
   puts "would you like to walk or run?"
   user_answer = gets.chomp
 
   if user_answer == "walk"
     distance += 1
+    energy += 1
     puts "distance from home is #{distance}km"
   elsif user_answer == "run"
     distance += 5
+    energy -= 20
     puts "distance from home is #{distance}km"
   elsif user_answer == "go home"
     home = false
     puts "distance from home is #{distance}km"
   else
     puts "wrong command"
+  end
+  if energy == 0
+    puts "not enough energy"
+    break
   end
 end
